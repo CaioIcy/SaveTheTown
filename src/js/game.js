9322cfd60272ac2066de56208ee382f677a23a0a
Caio Nardelli Maranhao <caio.nardelli@hotmail.com>
2013-09-28 01:27:02 -0300

    separate initialize

--- src/js/game.js
+++ src/js/game.js
@@ -1,14 +1,7 @@
 canvas=document.getElementById("canvas");
 d = canvas.getContext("2d");
 
-function initialize() {
-	player = new Player(230,230,0.001,4.71,390,60);
-	gate = new Array();
-	gate[0] = new Gate(X_POSITION_GATE_1,Y_POSITION_GATE_1,HEALTH_GATE);
-	gate[1] = new Gate(X_POSITION_GATE_2,Y_POSITION_GATE_2,HEALTH_GATE);
-	gate[2] = new Gate(X_POSITION_GATE_3,Y_POSITION_GATE_3,HEALTH_GATE);
-	gate[3] = new Gate(X_POSITION_GATE_4,Y_POSITION_GATE_4,HEALTH_GATE);
-}
+var walk = false;
 
 function update(){
 	keyInput();
@@ -43,6 +36,5 @@ function render(){
 	
 }
 
-window.onload=initialize;
 window.setInterval("update()",60/1000);
 window.setInterval("render()",1);
---
+++ src/js/initialize.js
@@ -0,0 +1,8 @@
+
+player = new Player(230,230,0.001,4.71,390,60);
+
+gate = new Array();
+gate[0] = new Gate(X_POSITION_GATE_1,Y_POSITION_GATE_1,HEALTH_GATE);
+gate[1] = new Gate(X_POSITION_GATE_2,Y_POSITION_GATE_2,HEALTH_GATE);
+gate[2] = new Gate(X_POSITION_GATE_3,Y_POSITION_GATE_3,HEALTH_GATE);
+gate[3] = new Gate(X_POSITION_GATE_4,Y_POSITION_GATE_4,HEALTH_GATE);
