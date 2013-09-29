f29e1c0ce7d0eaee7963f6b858bc56da951796f9
Simião <simiaosimis@hotmail.com>
2013-09-29 19:36:01 -0300

    Fixing menu bugs

--- src/js/Menu.js
+++ src/js/Menu.js
@@ -1,4 +1,6 @@
 
+	var allowMenu = 0;
+	
 function menu(event){
 
 var menuImage = new Image();
@@ -9,8 +11,7 @@ aboutImage.src = "res/images/about.png";
 
 var creditsImage  = new Image();
 creditsImage.src = "res/images/credit.png";
-
-
+	if( allowMenu == 0)
 	menuImage.onload = function(){d.drawImage(menuImage , 0, 0, canvas.width, canvas.height);};
 
 	var clickX = event.clientX;
@@ -28,13 +29,14 @@ creditsImage.src = "res/images/credit.png";
 		creditsImage.onload = function(){d.drawImage(creditsImage , 0, 0, canvas.width, canvas.height);};
 	}	
 		
-	if(clickX>590 && clickX<772 && clickY>256 && clickY<347){
-		
+	if(clickX>590 && clickX<772 && clickY>256 && clickY<347 && allowMenu == 0){
 		d.clearRect(0, 0, canvas.width, canvas.height);
-		
-		window.setInterval("update()",60/1000);
-		window.setInterval("render()",1);
-		
+
+			allowMenu = 1;
+
+			window.setInterval("update()",60/1000);
+			window.setInterval("render()",1);
+
 	}
 	
 }
